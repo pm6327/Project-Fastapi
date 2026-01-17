333a3cc42112128fdda29fa91694334559ba813f
Prakhar Maheshwari <prakharmaheshwari33@gmail.com>
2026-01-17 10:12:31 +0530

    Config and Security file in app folder

---
+++ app/core/config.py
@@ -0,0 +1,17 @@
+# Loads enviornment variables and app-wide Settings
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+    PROJECT_NAME = "Car Price API"
+    API_KEY = os.getenv("API_KEY", "demo-key")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_key", "Secret")
+    JWT_ALGORITHM = "HS256"
+    REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+    MODEL_PATH = "app/core/model/model.pkl"
+
+
+settings = Settings()
